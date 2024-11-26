97b8c379317682d77df85fc19cb724790db258ef
vodoprovod <olegbobrovnik228@gmail.com>
2024-11-26 18:22:26 +0200

    finished redact files

---
+++ src/js/1-timer.js
@@ -0,0 +1,75 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const btn = document.querySelector('[data-start]');
+let userSelectedDate = null;
+let interval;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+      
+      if (selectedDate <= new Date()) {
+           iziToast.error({
+          title: 'Error',
+          iconUrl: `../img/bix.png`,
+          message: 'Please choose a date in the future',
+          backgroundColor:`#ef4040`,
+        messageColor:`white`,
+        position:`topRight`,
+        titleColor:`white`
+        });
+        btn.disabled = true;
+      } else {
+        userSelectedDate = selectedDate;
+        btn.disabled = false;
+      }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+btn.addEventListener('click', function () {
+    if (userSelectedDate) {
+      const timeDifference = userSelectedDate - new Date();
+      const { days, hours, minutes, seconds } = convertMs(timeDifference);
+        (days, hours, minutes, seconds);
+        btn.disabled = true;
+
+      interval = setInterval(() => {
+        const { days, hours, minutes, seconds } = convertMs(userSelectedDate - new Date());
+        updateTimer(days, hours, minutes, seconds);
+      }, 1000);
+    }
+})
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+ function updateTimer(days, hours, minutes, seconds) {
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+  }
+
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  } 
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,54 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+  const delay = parseInt(this.querySelector('[name="delay"]').value, 10);
+  const state = this.querySelector('[name="state"]:checked').value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+        console.log(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(delay);
+        console.log(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+    promise.then(
+    (result) => {
+      iziToast.success({
+        title: 'Ok',
+        message: `Fulfilled promise in ${result}ms`,
+        position:`topRight`,
+        maxWidth:`383px`,
+        color: `#59a10d`,
+        titleColor:`white`,
+        iconUrl:`../img/Group.png`,
+        messageColor:`white`,
+
+      });
+    
+    },
+    (result) => {
+      iziToast.error({
+        title: 'Error',
+        message: ` Rejected promise in ${result}ms`,
+        position:`topRight`,
+        maxWidth:`302px`,
+        color: `#ef4040`,
+        titleColor:`white`,
+        iconUrl:`../img/bix.png`,
+        messageColor:`white`,
+    });
+    }
+    ); 
+
+    form.reset();
+})
